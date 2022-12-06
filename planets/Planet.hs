19dbc3b5439e5063881a58fba404ecf4352ada0c
romes <rodrigo.m.mesquita@gmail.com>
2022-12-06 00:59:09 +0000

    Mask from first layer when checkbox is ticked

--- planets/Planet.hs
+++ planets/Planet.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE BlockArguments #-}
 module Planet where
 
+import qualified Data.List.NonEmpty as NE
 import Data.String
 import Data.List (foldl')
 import GHC.Float
@@ -23,7 +25,8 @@ import Ghengin.Component.UI
 data PlanetSettings = PlanetSettings { resolution :: !(IORef Int)
                                      , radius     :: !(IORef Float)
                                      , color      :: !(IORef Vec3)
-                                     , noiseSettings :: ![NoiseSettings]
+                                     , useFirstLayerAsMask :: !(IORef Bool)
+                                     , noiseSettings :: !(NE.NonEmpty NoiseSettings)
                                      }
 data NoiseSettings = NoiseSettings { numLayers :: !(IORef Int)
                                    , strength  :: !(IORef Float)
@@ -66,36 +69,43 @@ instance UISettings PlanetSettings where
     resR   <- newIORef 5
     radR   <- newIORef 1
     colorR <- newIORef (vec3 1 0 0)
+    boolR  <- newIORef False
     ns1    <- makeSettings @NoiseSettings
     ns2    <- makeSettings @NoiseSettings
-    pure $ PlanetSettings resR radR colorR [ns1, ns2]
+    pure $ PlanetSettings resR radR colorR boolR [ns1, ns2]
 
-  makeComponents (PlanetSettings re ra co nss) =
+  makeComponents (PlanetSettings re ra co bo nss) =
     [ WithTree "Planet" [ SliderInt "Resolution" re 2 200
                         , SliderFloat "Radius" ra 0 3
                         , ColorPicker "Color" co
+                        , Checkbox "Mask" bo
                         ]
     ]
     -- Careful! The tree's cannot have the same Id otherwise they will behave
     -- the same.
-    <> map (\(ns, i) -> WithTree ("Layer " <> fromString (show i)) $ makeComponents ns) (zip nss [1..])
+    <> NE.toList (fmap (\(ns, i) -> WithTree ("Layer " <> fromString (show i)) $ makeComponents ns) (NE.zip nss [1..]))
     -- <> concatMap makeComponents nss
 
 newPlanet :: PlanetSettings -> Renderer Mesh
-newPlanet (PlanetSettings re ra co nss) = do
+newPlanet (PlanetSettings re ra co bo nss) = do
   re' <- get re
   ra' <- get ra
   co' <- get co
+  enableMask <- get bo
 
 
   let UnitSphere vs is = newUnitSphere re' (Just co')
 
   ps' <- forM vs $ \(Vertex p _ _) -> do
-    noiseElevation <- foldM (\acc (NoiseSettings nl st ro bro pers ce mv en) -> do
-                                    nv <- evaluateNoise <$> get nl <*> get st <*> get ro <*> get bro <*> get pers <*> get ce <*> get mv <*> get en <*> pure p
-                                    pure $ nv + acc
-                            ) 0 nss
-    pure $ p ^* (ra' * (1 + noiseElevation))
+    case nss of
+      NoiseSettings nl st ro bro pers ce mv en NE.:| nss' -> do
+        initialElevation <- evaluateNoise <$> get nl <*> get st <*> get ro <*> get bro <*> get pers <*> get ce <*> get mv <*> get en <*> pure p
+        let mask = if enableMask then initialElevation else 1
+        noiseElevation <- foldM (\acc (NoiseSettings nl st ro bro pers ce mv en) -> do
+                                        nv <- evaluateNoise <$> get nl <*> get st <*> get ro <*> get bro <*> get pers <*> get ce <*> get mv <*> get en <*> pure p
+                                        pure $ (nv * mask) + acc
+                                ) initialElevation nss'
+        pure $ p ^* (ra' * (1 + noiseElevation))
 
   let
       ns' = calculateSmoothNormals is ps'
@@ -130,7 +140,7 @@ evaluateNoise nlayers stren rough baseRoughness (float2Double -> persi) cent min
                                     freq'      = freq*rough -- >1 roughness will increase roughness as the layer increases
                                     amplitude' = amplitude*persi -- <1 persistence implies amplitude decreases with each layer
                                  in (noiseVal', freq', amplitude')
-                                ) (0,baseRoughness,1) [1..nlayers]
+                                ) (0,baseRoughness,1) ([1..nlayers] :: [Int])
    in if enabled then max (double2Float finalVal - minVal) 0 * stren else 0
 
 
