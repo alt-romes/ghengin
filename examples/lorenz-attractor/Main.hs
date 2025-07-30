3b4c641c7a9119195c2b5c86e0fdaf23f706af3d
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-07-31 00:11:35 +0100

    Init lorenz-attractor

---
+++ examples/lorenz-attractor/Main.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE PostfixOperators #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+module Main where
+
+import qualified Language.Haskell.TH.Syntax as TH
+import qualified Prelude
+import GHC.Generics
+import Geomancy.Vec3
+import Geomancy.Mat4
+import Geomancy.Transform
+import Geomancy.Vulkan.View
+import Geomancy.Vulkan.Projection
+import Ghengin.Core
+import Ghengin.Core.Shader.Data
+import Ghengin.Core.Mesh
+import Ghengin.Core.Material
+import Ghengin.Core.Prelude as Linear
+import Ghengin.Core.Render
+import Ghengin.Core.Render.Property
+import Ghengin.Core.Render.Pipeline
+import Ghengin.Core.Render.Queue
+import Data.List.Linear ()
+import qualified Data.Monoid.Linear as LMon
+import qualified Math.Linear as FIR
+import qualified FIR
+import qualified Data.Linear.Alias as Alias
+
+import Ghengin.Geometry.Cube (coloredCube)
+import Shaders
+
+type CubeMesh = Mesh '[Vec3, Vec3] '[Transform]
+
+data Camera
+  = Camera { view :: Mat4
+           , proj :: Mat4
+           }
+           deriving Generic
+           deriving anyclass Block
+
+defaultCamera :: Camera
+defaultCamera = Camera
+  { view = unTransform $ lookAt (vec3 0 0 0) (vec3 0 0 1) (vec3 0 1 0)
+  , proj = unTransform $ perspective @Int 45 0.1 1000 640 480
+  }
+
+gameLoop :: PipelineKey _ '[Camera] -- ^ rq key to camera
+         -> MeshKey _ _ _ _ '[Transform] -- ^ rq key to cube mesh
+         -> Float -- ^ rotation
+         -> Alias RenderPass
+          ⊸ RenderQueue ()
+          ⊸ Core (RenderQueue ())
+gameLoop ckey mkey rot rp rq = Linear.do
+ should_close <- (shouldCloseWindow ↑)
+ if should_close then (Alias.forget rp ↑) >> return rq else Linear.do
+  (pollWindowEvents ↑)
+
+  (rp, rq) <- render rp rq
+  rq <- (editMeshes mkey rq (traverse' $ propertyAt @0 (\(Ur tr) -> pure $ Ur $
+    scale 5 <> rotateY rot <> rotateX (-rot) <> translate 0 0 10)) ↑)
+
+  gameLoop ckey mkey (rot+0.01) rp rq
+
+main :: Prelude.IO ()
+main = do
+ withLinearIO $
+  runCore (640, 480) Linear.do
+
+    (rp1, rp2) <- (Alias.share =<< createSimpleRenderPass ↑)
+    pipeline <- (makeRenderPipeline rp1 shaderPipeline (StaticBinding (Ur defaultCamera) :## GHNil) ↑)
+    (emptyMat, pipeline) <- (material GHNil pipeline ↑)
+    (mesh :: CubeMesh, pipeline) <-
+      -- Also displays how TH can be used to create procedural meshes at compile time when the parameters are statically known
+      (createMesh pipeline (DynamicBinding (Ur (rotateY (pi/4))) :## GHNil) ($$(TH.liftTyped coloredCube)) ↑)
+    (rq, Ur pkey)    <- pure (insertPipeline pipeline LMon.mempty)
+    (rq, Ur mkey)    <- pure (insertMaterial pkey emptyMat rq)
+    (rq, Ur mshkey)  <- pure (insertMesh mkey mesh rq)
+
+    rq <- gameLoop pkey mshkey 0 rp2 rq
+
+    (freeRenderQueue rq ↑)
+
+    return (Ur ())
+
+
+-- non-compositional instance for "Transform", just for demo
+instance ShaderData Transform where
+  type FirType Transform = FIR.Struct '[ "m" 'FIR.:-> FIR.M 4 4 Float ]
+
+instance ShaderData Camera where
+  type FirType Camera = FIR.Struct '[ "view_matrix" 'FIR.:-> FIR.M 4 4 Float
+                                    , "proj_matrix" 'FIR.:-> FIR.M 4 4 Float ]
---
+++ examples/lorenz-attractor/Shaders.hs
@@ -0,0 +1,67 @@
+{-# OPTIONS_GHC -Wno-missing-local-signatures #-}
+
+{-# LANGUAGE CPP                   #-}
+{-# LANGUAGE BlockArguments        #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE OverloadedLabels      #-}
+{-# LANGUAGE RebindableSyntax      #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE NamedWildCards        #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE ViewPatterns          #-}
+
+module Shaders where
+
+-- fir
+import FIR
+import FIR.Syntax.Labels
+import Math.Linear
+
+-- ghengin
+import qualified Ghengin.Core.Shader as G
+
+-- See FIR.Validation.Layout about locations/component layouts...
+type VertexInput
+  = '[ Slot 0 0 ':-> V 3 Float
+     , Slot 1 0 ':-> V 3 Float ]
+
+shaderPipeline :: G.ShaderPipeline _
+shaderPipeline
+  = G.ShaderPipeline (StructInput @VertexInput @(Triangle List))
+  G.:>-> vertex
+  G.:>-> fragment
+
+type VertexDefs =
+  '[ "in_position"  ':-> Input      '[ Location 1 ] (V 3 Float) -- position comes after color (location=1)
+   , "in_color"     ':-> Input      '[ Location 0 ] (V 3 Float) -- color comes first (location=0)
+   , "frag_color"   ':-> Output     '[ Location 0 ] (V 3 Float)
+   , "model"        ':-> Uniform    '[ DescriptorSet 2, Binding 0 ] (Struct '[ "m" ':-> M 4 4 Float ])
+   , "camera"       ':-> Uniform    '[ DescriptorSet 0, Binding 0 ]
+                          (Struct [ "view_matrix" ':-> M 4 4 Float
+                                  , "proj_matrix" ':-> M 4 4 Float
+                                  ])
+   ]
+
+-- Descriptor Set 0 : Pipeline properties (bound once per pipeline)
+-- Descriptor Set 1 : Material properties (bound once per material)
+-- Descriptor Set 2 : Mesh properties (bound once per mesh)
+
+vertex :: G.VertexShaderModule VertexDefs _
+vertex = shader do
+    ~(Vec3 x y z) <- #in_position
+    color         <- #in_color
+    proj_matrix   <- use @(Name "camera" :.: Name "proj_matrix")
+    view_matrix   <- use @(Name "camera" :.: Name "view_matrix")
+    model_mat     <- use @(Name "model" :.: Name "m")
+    #gl_Position .= (proj_matrix !*! view_matrix !*! model_mat) !*^ (Vec4 x y z 1)
+    #frag_color  .= color
+
+fragment :: G.FragmentShaderModule '["in_color" ':-> Input '[Location 0] (V 3 Float)] _
+fragment = shader do
+  ~(Vec3 r g b) <- #in_color
+  #out_colour .= Vec4 r g b 1
+
