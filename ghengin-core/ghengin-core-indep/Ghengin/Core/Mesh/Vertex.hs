26dc573c54caf6bbf5f262b99bb1a91975fa2217
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-11-22 19:30:05 +0000

    Use deriving via Packed (Vertex ...) for Vertex Data
    Vertex attribute data should be packed rather than use weird alignments (save those for uniform shader data)
    
    Fixes #17

--- ghengin-core/ghengin-core-indep/Ghengin/Core/Mesh/Vertex.hs
+++ ghengin-core/ghengin-core-indep/Ghengin/Core/Mesh/Vertex.hs
@@ -101,12 +101,11 @@ instance (Block (Vertex (y:ys)), Block x) => Block (Vertex (x:y:ys)) where
     writePacked p (Diff o) (Sin a)
     writePacked p (Diff $ o + sizeOfPacked (Proxy @(Vertex '[x]))) b
 
--- NB: We use Std140 for Storable Vertex, which isn't quite right since
--- vertices need to abide by the location/component layout, but is fine for now.
---
+-- NB: We use Packed for Storable Vertex
 -- A good explanation is in https://developer.apple.com/library/archive/documentation/3DDrawing/Conceptual/OpenGLES_ProgrammingGuide/TechniquesforWorkingwithVertexData/TechniquesforWorkingwithVertexData.html
-deriving via (Std140 (Vertex '[x])) instance Block x => Storable (Vertex '[x])
-deriving via (Std140 (Vertex (x:y:ys))) instance (Block x, Block (Vertex (y:ys))) => Storable (Vertex (x:y:ys))
+-- See also https://github.com/alt-romes/ghengin/pull/16#issuecomment-3563044060
+deriving via (Graphics.Gl.Block.Packed (Vertex '[x])) instance Block x => Storable (Vertex '[x])
+deriving via (Graphics.Gl.Block.Packed (Vertex (x:y:ys))) instance (Block x, Block (Vertex (y:ys))) => Storable (Vertex (x:y:ys))
 
 instance Show x => Show (Vertex '[x]) where
   show (Sin x) = show x
