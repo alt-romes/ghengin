982ac93bd7d3f2332a039f2df97dbfacbeff594f
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-11-23 19:31:15 +0000

    Allow creating the mesh from an already loaded 'WavefrontOBJ'
    Useful if embedding the .obj using TH

--- ghengin/ghengin-geometry/Ghengin/Geometry/Obj.hs
+++ ghengin/ghengin-geometry/Ghengin/Geometry/Obj.hs
@@ -6,7 +6,6 @@ module Ghengin.Geometry.Obj where
 
 import Prelude
 import qualified Prelude.Linear as Linear
-import Control.Monad
 
 import qualified Data.Vector as V
 
@@ -15,17 +14,22 @@ import Geomancy
 import Codec.Wavefront
 
 import Ghengin.Core.Type.Compatible
-import Ghengin.Core.Prelude (GHList(..))
 import Ghengin.Core.Render.Pipeline
 import Ghengin.Core.Render.Property
 import Ghengin.Core.Renderer
 import Ghengin.Core.Mesh
-import Ghengin.Core.Mesh.Vertex
 import qualified Data.Linear.Alias as Alias
 
 import qualified Control.Functor.Linear as Linear
 import qualified Control.Monad.IO.Class.Linear as Linear
 
+-- | Loads a 'WavefrontOBJ' from a file.
+--
+-- Useful if one desires to embed the model with template haskell
+loadObjFile :: FilePath -> IO (Either String WavefrontOBJ)
+loadObjFile = fromFile
+
+-- | Load an .obj file and create a mesh from it.
 loadObjMesh :: (CompatibleMesh ts π, CompatibleVertex '[Vec3, Vec3] π)
             => FilePath
             -> RenderPipeline π ps
@@ -34,27 +38,38 @@ loadObjMesh :: (CompatibleMesh ts π, CompatibleVertex '[Vec3, Vec3] π)
             -- The property bindings need to set when creating it.
              ⊸ Renderer (Mesh '[Vec3, Vec3] ts, RenderPipeline π ps)
 loadObjMesh filepath rp props = Linear.do
-  Linear.liftSystemIOU (fromFile filepath) Linear.>>= \case
+  Linear.liftSystemIOU (loadObjFile filepath) Linear.>>= \case
     Linear.Ur (Left err) -> Linear.do
       destroyRenderPipeline rp
       Alias.forget props
       Linear.liftSystemIO (fail err)
     Linear.Ur (Right wavefrontObj) ->
-      let
-          locs    = V.map getLoc    $ wavefrontObj.objLocations
-          normals = V.map getNormal $ wavefrontObj.objNormals
-          faces   = V.map (.elValue) wavefrontObj.objFaces
+      createObjMesh wavefrontObj rp props
 
-          getLoc :: Location -> Vec3
-          getLoc (Location x y z _) = vec3 x (-y) z
+-- | Create a mesh from a 'WavefrontOBJ'
+createObjMesh :: (CompatibleMesh ts π, CompatibleVertex '[Vec3, Vec3] π)
+              => WavefrontOBJ
+              -> RenderPipeline π ps
+               ⊸ PropertyBindings ts
+              -- ^ These must NOT be added after creating the mesh.
+              -- The property bindings need to set when creating it.
+               ⊸ Renderer (Mesh '[Vec3, Vec3] ts, RenderPipeline π ps)
+createObjMesh wavefrontObj rp props =
+  let
+      locs    = V.map getLoc    $ wavefrontObj.objLocations
+      normals = V.map getNormal $ wavefrontObj.objNormals
+      faces   = V.map (.elValue) wavefrontObj.objFaces
 
-          getNormal :: Normal -> Vec3
-          getNormal (Normal x y z) = vec3 x y z
+      getLoc :: Location -> Vec3
+      getLoc (Location x y z _) = vec3 x (-y) z
 
-          vertices = V.zipWith (:&:) locs normals
-          ixs = V.concatMap (\(Face a b c d) -> V.fromList $
-                  [ a.faceLocIndex , b.faceLocIndex , c.faceLocIndex ] ++ map (.faceLocIndex) d
-                  ) faces
-       in
-          createMeshWithIxsSV rp props (V.convert vertices) (V.convert $ V.map fromIntegral ixs)
+      getNormal :: Normal -> Vec3
+      getNormal (Normal x y z) = vec3 x y z
+
+      vertices = V.zipWith (:&:) locs normals
+      ixs = V.concatMap (\(Face a b c d) -> V.fromList $
+              [ a.faceLocIndex , b.faceLocIndex , c.faceLocIndex ] ++ map (.faceLocIndex) d
+              ) faces
+   in
+      createMeshWithIxsSV rp props (V.convert vertices) (V.convert $ V.map fromIntegral ixs)
 
