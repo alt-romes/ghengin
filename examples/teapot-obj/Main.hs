10c09d3d814856f62a18c6a7a9c625307bdd31f4
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-11-24 00:13:35 +0000

    Add teapot object example
    Needs cull-mode = None, or Front for good results
    
    Does that mean we're doing triangle order wrong?! Try more meshes...

---
+++ examples/teapot-obj/Main.hs
@@ -0,0 +1,72 @@
+{-# LANGUAGE PartialTypeSignatures #-}
+module Main where
+
+import Ghengin.Core.Prelude as Linear
+import qualified Prelude
+
+import Geomancy
+import Geomancy.Transform
+import Ghengin.Core
+import Ghengin.Core.Shader.Data
+import Ghengin.Core.Mesh
+import Ghengin.Core.Material
+import Ghengin.Core.Render
+import Ghengin.Core.Render.Property
+import Ghengin.Core.Render.Pipeline
+import Ghengin.Core.Render.Queue
+
+import qualified FIR
+import qualified Math.Linear as FIR
+
+import qualified Data.Monoid.Linear as LMon
+import qualified Data.Linear.Alias as Alias
+
+-- ghengin
+import Ghengin.Camera
+import Ghengin.Geometry.Obj
+
+import Shaders
+
+main :: Prelude.IO ()
+main = do
+ withLinearIO $
+  runCore (640, 480) Linear.do
+
+    (rp1, rp2) <- (Alias.share =<< createSimpleRenderPass ↑)
+
+    pipeline <- (makeRenderPipelineWith defaultGraphicsPipelineSettings{cullMode=CullNone} rp1 shaderPipeline (StaticBinding (Ur camera) :## GHNil) ↑)
+    (emptyMat, pipeline) <- (material GHNil pipeline ↑)
+    (mesh, pipeline) <- (loadObjMesh "examples/teapot-obj/assets/teapot.obj" pipeline
+                          (DynamicBinding (Ur (scale 1.2)) :## GHNil) ↑)
+
+    let !(rq, Ur pkey) = insertPipeline pipeline LMon.mempty
+    (rq, Ur mkey)    <- pure (insertMaterial pkey emptyMat rq)
+    (rq, Ur mshkey)  <- pure (insertMesh mkey mesh rq)
+
+    rq <- gameLoop mshkey rp2 rq
+
+    (freeRenderQueue rq ↑)
+
+    return (Ur ())
+
+gameLoop :: MeshKey _ _ _ _ '[Transform] -- ^ rq key to cube mesh
+         -> Alias RenderPass
+          ⊸ RenderQueue ()
+          ⊸ Core (RenderQueue ())
+gameLoop mkey rp rq = Linear.do
+ should_close <- (shouldCloseWindow ↑)
+ if should_close then (Alias.forget rp ↑) >> return rq else Linear.do
+  (pollWindowEvents ↑)
+
+  (rp, rq) <- render rp rq
+  rq <- (editMeshes mkey rq (traverse' $ propertyAt @0 (\(Ur tr) -> pure $ Ur $
+    rotateY 0.01 <> tr)) ↑)
+
+  gameLoop mkey rp rq
+
+camera :: Camera "view_matrix" "proj_matrix"
+camera = cameraLookAt (vec3 0 (-5) (-5){- move camera up and back-}) (vec3 0 0 0) (1280, 720)
+
+-- non-compositional instance for "Transform", just for demo
+instance ShaderData Transform where
+  type FirType Transform = FIR.Struct '[ "m" 'FIR.:-> FIR.M 4 4 Float ]
---
+++ examples/teapot-obj/Shaders.hs
@@ -0,0 +1,86 @@
+{-# OPTIONS_GHC -Wno-missing-local-signatures #-}
+
+{-# LANGUAGE CPP                   #-}
+{-# LANGUAGE BlockArguments        #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE OverloadedLabels      #-}
+{-# LANGUAGE RebindableSyntax      #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE NamedWildCards        #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE ViewPatterns          #-}
+
+module Shaders where
+
+-- fir
+import FIR
+import FIR.Syntax.Labels
+import Math.Linear
+
+-- ghengin
+import qualified Ghengin.Core.Shader as G
+import Ghengin.Camera.Shader.Lighting
+
+-- See FIR.Validation.Layout about locations/component layouts...
+type VertexInput
+  = '[ Slot 0 0 ':-> V 3 Float
+     , Slot 1 0 ':-> V 3 Float ]
+
+shaderPipeline :: G.ShaderPipeline _
+shaderPipeline
+  = G.ShaderPipeline (StructInput @VertexInput @(Triangle List))
+  G.:>-> vertex
+  G.:>-> fragment
+
+type VertexDefs =
+  '[ "in_position"  ':-> Input      '[ Location 0 ] (V 3 Float)
+   , "in_normal"    ':-> Input      '[ Location 1 ] (V 3 Float)
+   , "out_position" ':-> Output     '[ Location 0 ] (V 4 Float)
+   , "out_normal"   ':-> Output     '[ Location 1 ] (V 4 Float)
+
+    -- Descriptor Set 0 : Pipeline properties (bound once per pipeline)
+   , "camera"       ':-> Uniform    '[ DescriptorSet 0, Binding 0 ]
+                          (Struct [ "view_matrix" ':-> M 4 4 Float
+                                  , "proj_matrix" ':-> M 4 4 Float
+                                  ])
+    -- Descriptor Set 1 : Material properties (bound once per material)
+    -- ...
+
+    -- Descriptor Set 2 : Mesh properties (bound once per mesh)
+   , "model"        ':-> Uniform    '[ DescriptorSet 2, Binding 0 ] (Struct '[ "m" ':-> M 4 4 Float ])
+   ]
+
+
+vertex :: G.VertexShaderModule VertexDefs _
+vertex = shader do
+    ~(Vec3 x y z)    <- #in_position
+    ~(Vec3 nx ny nz) <- #in_normal
+    proj_matrix <- use @(Name "camera" :.: Name "proj_matrix")
+    view_matrix <- use @(Name "camera" :.: Name "view_matrix")
+    model_mat   <- use @(Name "model" :.: Name "m")
+    let view_p = (view_matrix !*! model_mat) !*^ (Vec4 x y z 1)
+    #gl_Position  .= proj_matrix !*^ view_p
+    #out_position .= view_p
+    #out_normal   .= (view_matrix !*! model_mat) !*^ (Vec4 nx ny nz 0) -- careful! don't translate (hence the w=0)
+
+fragment :: G.FragmentShaderModule
+  '[ "in_position"  ':-> Input      '[ Location 0 ] (V 4 Float) -- in view space
+   , "in_normal"    ':-> Input      '[ Location 1 ] (V 4 Float) -- in view space
+   ] _
+fragment = shader do
+  let lightDir  = Vec3 0 (-1) 0
+  let lightCol  = Vec3 0.3 0.3 0.3
+  let objectCol = Vec3 1 1 1
+  let shininess = 16
+
+  lightValue <- blinnPhong 0.05 shininess lightDir lightCol
+
+  let Vec3 colx coly colz
+        = gammaCorrection defaultGamma (lightValue `pointwiseMult` objectCol)
+
+  #out_colour .= Vec4 colx coly colz 1
+
