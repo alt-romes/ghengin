6f81002f7976459315e5129bfd722c177a44a31a
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-06-21 12:23:18 +0100

    Clean up Vulkan.Renderer.Sampler

--- ghengin-core/ghengin-vulkan/Ghengin/Vulkan/Renderer/Sampler.hs
+++ ghengin-core/ghengin-vulkan/Ghengin/Vulkan/Renderer/Sampler.hs
@@ -2,46 +2,88 @@
 module Ghengin.Vulkan.Renderer.Sampler
   (
   -- * Sampler
-    Sampler(..)
+    Sampler
   , createSampler
   , destroySampler
 
-  -- * Abstract filter types
+  -- * Options
   , SamplerFilter(..)
-  , filterToVk
-
-  -- * Abstract address mode types
   , SamplerAddressMode(..)
-  , addressModeToVk
-
   ) where
 
 import Ghengin.Core.Log
 import Ghengin.Core.Prelude as Linear
+import Ghengin.Vulkan.Renderer.Kernel
+import qualified Data.Linear.Alias as Alias
 
 import qualified Vulkan.Zero as Vk
 import qualified Vulkan as Vk
 import qualified Unsafe.Linear as Unsafe
 
-import Ghengin.Vulkan.Renderer.Kernel
+--------------------------------------------------------------------------------
 
-import qualified Data.Linear.Alias as Alias
+type Sampler = Vk.Sampler
 
 data SamplerFilter
   = FilterNearest
   | FilterLinear
 
--- | Convert abstract filter to Vulkan filter
-filterToVk :: SamplerFilter -> Vk.Filter
-filterToVk FilterNearest = Vk.FILTER_NEAREST
-filterToVk FilterLinear = Vk.FILTER_LINEAR
-
+-- | SamplerAddressMode (VK_SAMPLER_ADDRESS_MODE_* in Vulkan)
+--
+-- * SamplerRepeat: Repeat the texture when going beyond the image dimensions.
+-- * SamplerMirroredRepeat: Like repeat, but inverts the coordinates to mirror the image when going beyond the dimensions.
+-- * SamplerClampToEdge:       Take the color of the edge closest to the coordinate beyond the image dimensions.
+-- * SamplerMirrorClampToEdge: Like clamp to edge, but instead uses the edge opposite to the closest edge.
+-- * SamplerClampToBorder:    Return a solid color when sampling beyond the dimensions of the image.
 data SamplerAddressMode
   = SamplerRepeat
   | SamplerMirroredRepeat
   | SamplerClampToEdge
   | SamplerMirrorClampToEdge
   | SamplerClampToBorder
+
+-- | Create a sampler with the given filter and sampler address mode
+createSampler :: SamplerFilter -> SamplerAddressMode -> Renderer (Alias Sampler)
+createSampler filter addrMode = enterD "Creating Sampler" Linear.do
+  let info = Vk.SamplerCreateInfo
+        { magFilter = filterToVk filter
+        , minFilter = filterToVk filter
+
+        , addressModeU = addressModeToVk addrMode
+        , addressModeV = addressModeToVk addrMode
+        , addressModeW = addressModeToVk addrMode
+
+        , anisotropyEnable = False
+        , maxAnisotropy = 1 -- Could query for MAX
+
+        , borderColor = Vk.BORDER_COLOR_INT_OPAQUE_BLACK
+        , unnormalizedCoordinates = False
+        , compareEnable = False
+        , compareOp = Vk.COMPARE_OP_ALWAYS
+
+        , mipmapMode = Vk.SAMPLER_MIPMAP_MODE_LINEAR
+        , mipLodBias = 0
+        , minLod = 0
+        , maxLod = 0
+
+        , flags = Vk.zero
+        , next = ()
+        }
+
+  vkSampler <- unsafeUseDevice (\dev -> Vk.createSampler dev info Nothing)
+  refc <- Alias.newAlias destroySampler vkSampler
+  pure refc
+
+destroySampler :: Sampler ⊸ Renderer ()
+destroySampler s = Unsafe.toLinear (\s' -> enterD "destroySampler" $ unsafeUseDevice (\dev -> Vk.destroySampler dev s' Nothing)) s
+
+--------------------------------------------------------------------------------
+-- * Conversion utilities
+
+-- | Convert abstract filter to Vulkan filter
+filterToVk :: SamplerFilter -> Vk.Filter
+filterToVk FilterNearest = Vk.FILTER_NEAREST
+filterToVk FilterLinear = Vk.FILTER_LINEAR
 
 -- | Convert abstract address mode to Vulkan address mode
 addressModeToVk :: SamplerAddressMode -> Vk.SamplerAddressMode
@@ -50,52 +92,3 @@ addressModeToVk SamplerMirroredRepeat = Vk.SAMPLER_ADDRESS_MODE_MIRRORED_REPEAT
 addressModeToVk SamplerClampToEdge = Vk.SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE
 addressModeToVk SamplerMirrorClampToEdge = Vk.SAMPLER_ADDRESS_MODE_MIRROR_CLAMP_TO_EDGE
 addressModeToVk SamplerClampToBorder = Vk.SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER
-
-newtype Sampler = Sampler { sampler :: Vk.Sampler }
-
--- | Create a sampler with the given filter and sampler address mode
---
--- * VK_FILTER_NEAREST
--- * VK_FILTER_LINEAR
---
--- * VK_SAMPLER_ADDRESS_MODE_REPEAT: Repeat the texture when going beyond the image dimensions.
--- * VK_SAMPLER_ADDRESS_MODE_MIRRORED_REPEAT: Like repeat, but inverts the coordinates to mirror the image when going beyond the dimensions.
--- * VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE: Take the color of the edge closest to the coordinate beyond the image dimensions.
--- * VK_SAMPLER_ADDRESS_MODE_MIRROR_CLAMP_TO_EDGE: Like clamp to edge, but instead uses the edge opposite to the closest edge.
--- * VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER: Return a solid color when sampling beyond the dimensions of the image.
-createSampler :: SamplerFilter -> SamplerAddressMode -> Renderer (Alias Sampler)
-createSampler filter addrMode = enterD "Creating Sampler" Linear.do
-  let
-      -- TODO: Make more flexible as needed
-      info = Vk.SamplerCreateInfo { magFilter = filterToVk filter
-                                  , minFilter = filterToVk filter
-
-                                  , addressModeU = addressModeToVk addrMode
-                                  , addressModeV = addressModeToVk addrMode
-                                  , addressModeW = addressModeToVk addrMode
-
-                                  , anisotropyEnable = False
-                                  , maxAnisotropy = 1 -- Could query for MAX
-
-                                  , borderColor = Vk.BORDER_COLOR_INT_OPAQUE_BLACK
-                                  , unnormalizedCoordinates = False
-                                  , compareEnable = False
-                                  , compareOp = Vk.COMPARE_OP_ALWAYS
-
-                                  , mipmapMode = Vk.SAMPLER_MIPMAP_MODE_LINEAR
-                                  , mipLodBias = 0
-                                  , minLod = 0
-                                  , maxLod = 0
-
-                                  , flags = Vk.zero
-                                  , next = ()
-                                  }
-
-  vkSampler <- unsafeUseDevice (\dev -> Vk.createSampler dev info Nothing)
-  refc <- Alias.newAlias destroySampler (Sampler vkSampler)
-  pure refc
-
-
-destroySampler :: Sampler ⊸ Renderer ()
-destroySampler (Sampler s) = Unsafe.toLinear (\s' -> enterD "destroySampler" $ unsafeUseDevice (\dev -> Vk.destroySampler dev s' Nothing)) s
-
